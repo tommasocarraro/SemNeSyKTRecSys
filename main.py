38d52eda6e5dc9e8d1f25f36ac92a90a8495829b
tommasocarraro <tommasocarraro96@gmail.com>
2023-12-19 11:51:28 +0100

    metadata is now cleaned

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+# STATISTICS METADATA
+# start: 363876 matched and 116159 without title
+# first scraping: 456341 matched and 441 are captcha-or-DOM and 23253 are 404 error
+# end: 467833 matched and 11723 are 404 error and 479 are captcha in wayback -> captcha are more because some of the
+# 404 error before became a captcha after Wayback Machine. Note also that about 70 items with a DOM problem have to be
+# manually inserted in the file because Soup was not finding an ID that was actually there
+
 import os
 
 from nesy.data import create_asin_metadata_json, create_pandas_dataset,\
-    entity_linker_api, entity_linker_api_query, metadata_scraping, filter_metadata, metadata_stats
+    entity_linker_api, entity_linker_api_query, metadata_scraping, filter_metadata, metadata_stats, metadata_cleaning
 import json
 import pandas as pd
 
@@ -20,8 +27,10 @@ if __name__ == "__main__":
     # todo completare e verificare che sia tutto ok e perche' non mi carica i problemi di DOM correttamente
     # metadata_scraping("./data/processed/final-metadata.json", 1,
     #                   motivation="DOM", save_tmp=True, batch_size=20, wayback=True)
-    metadata_stats("./data/processed/filtered-metadata.json", errors=["no-title"], save_asins=False)
-    metadata_stats("./data/processed/final-metadata.json", errors=["404-error", "DOM", "captcha"], save_asins=False)
+    # metadata_stats("./data/processed/filtered-metadata.json", errors=["no-title"], save_asins=False)
+    # metadata_stats("./data/processed/complete-filtered-metadata.json", errors=["captcha-or-DOM", "404-error"], save_asins=False)
+    # metadata_stats("./data/processed/final-metadata.json", errors=["404-error", "DOM", "captcha"], save_asins=False)
+    metadata_cleaning("./data/processed/final-metadata.json")
     # # todo fare il contrario da wikidata ad amazon, come check che i match sono corretti
     # with open("./data/processed/mapping-reviews_Movies_and_TV_5.json") as json_file:
     #     mapping = json.load(json_file)
--- nesy/data.py
+++ nesy/data.py
@@ -401,6 +401,22 @@ def metadata_stats(metadata, errors, save_asins=True):
     print(errors)
 
 
+def metadata_cleaning(metadata):
+    """
+    This function takes as input a metadata file after all the scraping jobs have been completed (only 404 error and
+    matched titles should remain) and remove all the ASINs with 404 error.
+
+    The file is then overwritten.
+
+    :param metadata: metadata file that has to be cleaned
+    """
+    with open(metadata) as json_file:
+        m_data = json.load(json_file)
+    m_data = {k:v for k, v in m_data.items() if v != "404-error"}
+    with open(metadata, 'w', encoding='utf-8') as f:
+        json.dump(m_data, f, ensure_ascii=False, indent=4)
+
+
 def create_pandas_dataset(data):
     """
     This function takes as input a JSON file containing Amazon reviews and produces a CSV file. Each record has
