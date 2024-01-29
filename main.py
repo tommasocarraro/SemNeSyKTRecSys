023db1d26d3db698fcfb24ad589c831bfca248dc
tommasocarraro <tommasocarraro96@gmail.com>
2024-01-29 11:26:28 +0100

    first music mapping created
    Former-commit-id: 5f6f3b2a9c036e934c70eabd723cb0f13b12b65e

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ if __name__ == "__main__":
     # create_asin_metadata_json("./data/raw/metadata.json")
     # metadata_scraping("./data/processed/metadata.json")
     # create_pandas_dataset("./data/raw/reviews_CDs_and_Vinyl_5.json")
-    # entity_linker_api_query("./data/processed/reviews_Movies_and_TV_5.csv", True)
+    entity_linker_api_query("./data/processed/reviews_CDs_and_Vinyl_5.csv", True)
     # get_wid_labels("./data/raw/movies.json")
     # get_wid_labels("./data/raw/music.json")
     # get_wid_labels("./data/raw/books.json")
     # get_wid_per_cat("music")
-    metadata_scraping("./data/processed/complete-filtered-metadata.json", motivation="404-error", save_tmp=True,
-                      batch_size=100, mode="captcha")
+    # metadata_scraping("./data/processed/complete-filtered-metadata.json", motivation="404-error", save_tmp=True,
+    #                   batch_size=100, mode="captcha")
     # entity_linker_api_query("./data/processed/reviews_CDs_and_Vinyl_5.csv", use_dump=True, retry=False)
     # metadata_scraping("./data/processed/final-metadata.json", 1,
     #                   motivation="DOM", save_tmp=True, batch_size=20, wayback=True)
