71106e71677f232bb8a6320be7fc2369a55536e6
Nicolò Bertocco <nick.bertocco@gmail.com>
2024-05-08 12:52:35 +0200

    Improved preprocessing code
    Former-commit-id: 41783bbfa65d7aaad550a22e904e6ff3472cb241

--- data/kgtk_wrappers/cat.py
+++
@@ -1,6 +0,0 @@
-from kgtk.cli_entry import cli_entry
-
-
-def cat(input_graphs: list[str], output_path: str):
-    args = ["kgtk", "cat", "-i", *input_graphs, "-o", output_path]
-    cli_entry(*args)
