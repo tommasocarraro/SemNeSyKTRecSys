5adab43d86918ac639769737c35a9cc2946669a9
Nicolò Bertocco <nick.bertocco@gmail.com>
2024-05-28 14:57:37 +0200

    Improved rate limit handlind during throttling

--- nesy/dataset_augmentation/api_querying/get_request.py
+++ nesy/dataset_augmentation/api_querying/get_request.py
@@ -1,6 +1,7 @@
 from asyncio.exceptions import CancelledError
+from datetime import datetime
 from json import JSONDecodeError
-from typing import Union
+from typing import Union, Any
 
 import aiohttp
 from aiohttp import ClientResponseError
@@ -8,22 +9,44 @@ from aiolimiter import AsyncLimiter
 from backoff import expo, on_exception, random_jitter
 from loguru import logger
 
+last_time: Union[datetime, None] = None
 
-async def _handle_backoff(details):
+
+def _handle_backoff(details: dict[str, Any]):
     limiter: AsyncLimiter = details["kwargs"]["_limiter"]
-    logger.warning(
-        f"HTTP error 429 received. Setting limiter time period to {limiter.time_period} seconds"
-    )
-    limiter.time_period *= 2
+    logger.warning("HTTP error 429 received")
+    current_time = datetime.now()
+    decay_factor = 0.8
+    global last_time
+    if last_time is None or (current_time - last_time).total_seconds() >= 10:
+        logger.warning(
+            f"Decreasing rate per second from {limiter._rate_per_sec} to {limiter._rate_per_sec * decay_factor}"
+        )
+        last_time = current_time
+        limiter._rate_per_sec *= decay_factor
+
+
+def _handle_success(details: dict[str, Any]):
+    limiter: AsyncLimiter = details["kwargs"]["_limiter"]
+    growth_factor = 1.1
+    global last_time
+    current_time = datetime.now()
+    if last_time is not None and (current_time - last_time).total_seconds() >= 60:
+        logger.debug(
+            f"Increasing rate per second from {limiter._rate_per_sec} to {limiter._rate_per_sec * growth_factor}"
+        )
+        limiter._rate_per_sec *= growth_factor
 
 
 @on_exception(
     expo,
     ClientResponseError,
-    max_tries=8,
+    max_tries=5,
     on_backoff=_handle_backoff,
+    on_success=_handle_success,
     jitter=random_jitter,
-    giveup=lambda e: e.status not in [429, 503],
+    giveup=lambda e: e.status not in [429, 503],  # retry only on 429 and 503
+    logger=logger,
 )
 async def _fetch(
     url: str,
