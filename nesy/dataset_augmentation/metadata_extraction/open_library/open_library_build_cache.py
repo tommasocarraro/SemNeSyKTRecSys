195a2d9673da30b95a1552aee15b910243062cbf
Nicolò Bertocco <nick.bertocco@gmail.com>
2024-06-02 21:59:04 +0200

    querying open library almost done
    Former-commit-id: d64b5871cba8a1a91dcd95c9432a3698d7ef37b9

--- nesy/dataset_augmentation/metadata_extraction/open_library/open_library_build_cache.py
+++ nesy/dataset_augmentation/metadata_extraction/open_library/open_library_build_cache.py
@@ -6,12 +6,15 @@ from tqdm.auto import tqdm
 from config import PSQL_CONN_STRING, PSQL_CONN_STRING_SANS_DB
 
 
-def build_cache(authors_dump_path: str, editions_dump_path: str) -> None:
+def build_cache(
+    authors_dump_path: str, editions_dump_path: str, works_dump_path: str
+) -> None:
     """
     Imports authors and editions metadata from open library into a PostgreSQL database
     Args:
         authors_dump_path: preprocessed authors JSONL file path
         editions_dump_path: preprocessed editions JSONL file path
+        works_dump_path: preprocessed works JSONL file path
 
     Returns:
         None
@@ -33,7 +36,10 @@ def build_cache(authors_dump_path: str, editions_dump_path: str) -> None:
     c.execute("CREATE EXTENSION pg_trgm")
     c.execute("CREATE TABLE authors (key TEXT PRIMARY KEY, name TEXT)")
     c.execute(
-        "CREATE TABLE editions (key TEXT PRIMARY KEY, title VARCHAR(255), authors TEXT[], year CHAR(4))"
+        "CREATE TABLE editions (key TEXT PRIMARY KEY, title VARCHAR(200), authors TEXT[], year CHAR(4))"
+    )
+    c.execute(
+        "CREATE TABLE works (key TEXT PRIMARY KEY, title VARCHAR(200), authors TEXT[])"
     )
 
     # Process the authors dump
@@ -63,6 +69,19 @@ def build_cache(authors_dump_path: str, editions_dump_path: str) -> None:
                 )
     conn.commit()
 
+    # Process the works dump
+    with c.copy("COPY works (key, title, authors) FROM STDIN") as copy:
+        with open(works_dump_path, "r", encoding="utf-8") as work_file:
+            for line in tqdm(work_file, desc="Importing works...", dynamic_ncols=True):
+                work = json.loads(line)
+                copy.write_row(
+                    (
+                        work["key"],
+                        work["title"],
+                        work["authors"],
+                    )
+                )
+
     logger.info("Creating the materialized view...")
     c.execute(
         """
@@ -70,11 +89,24 @@ def build_cache(authors_dump_path: str, editions_dump_path: str) -> None:
         SELECT e.title, e.year, array_agg(a.name) AS authors
         FROM editions e
         JOIN authors a ON a.key = ANY (e.authors)
-        GROUP BY e.title, e.year"""
+        GROUP BY e.title, e.year
+        """
+    )
+    c.execute(
+        """
+        CREATE MATERIALIZED VIEW works_authors_materialized_view AS
+        SELECT w.title, array_agg(a.name) AS authors
+        FROM works w
+        JOIN authors a ON a.key = ANY (w.authors)
+        GROUP BY w.title
+        """
     )
     logger.info("Creating the materialized view index...")
     c.execute(
         "CREATE INDEX trgm_idx ON edition_authors_materialized_view USING gist (title gist_trgm_ops)"
+    )
+    c.execute(
+        "CREATE INDEX trgm_idx_2 ON works_authors_materialized_view USING gist (title gist_trgm_ops)"
     )
     conn.commit()
 
