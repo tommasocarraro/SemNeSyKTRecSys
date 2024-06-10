a80b87cd3acb14fe58d06c7bdd86e1676351273b
Nick <nick.bertocco@gmail.com>
2024-06-10 13:18:56 +0200

    refactoring fix

--- main_metadata_extraction.py
+++ main_metadata_extraction.py
@@ -1,6 +1,9 @@
 import os
 
-from nesy.dataset_augmentation.metadata_extraction.amazon_dataset import merge_metadata_for_wikidata, extract_metadata
+from nesy.dataset_augmentation.metadata_extraction.amazon_dataset import (
+    merge_metadata_for_wikidata,
+    extract_metadata,
+)
 
 
 def main():
