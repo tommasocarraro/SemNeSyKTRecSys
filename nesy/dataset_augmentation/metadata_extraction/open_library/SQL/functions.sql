cfaaac50209d5cb901729e44e40b541885b712d8
Nicolò Bertocco <nick.bertocco@gmail.com>
2024-06-15 19:39:49 +0200

    finished re-implementing open library api

--- nesy/dataset_augmentation/metadata_extraction/open_library/SQL/functions.sql
+++ nesy/dataset_augmentation/metadata_extraction/open_library/SQL/functions.sql
@@ -1,108 +1,109 @@
 CREATE FUNCTION get_book_info_by_isbn(isbn_param TEXT)
 RETURNS TABLE (
-    book_title VARCHAR(200),
-    book_year CHAR(4),
-    author_names TEXT[]
+    title VARCHAR(200),
+    person TEXT[],
+    year CHAR(4)
 )
 AS $$
 DECLARE
-    works_keys TEXT[];
-    edition_title VARCHAR(200);
-    work_title VARCHAR(200);
-    min_year CHAR(4);
-    author_keys TEXT[];
+    work_keys_var TEXT[];
+    edition_title_var VARCHAR(200);
+    work_title_var VARCHAR(200);
+    min_year_var CHAR(4);
+    author_keys_var TEXT[];
+    year_var CHAR(4);
 BEGIN
     -- Query by ISBN
     SELECT
-        title,
-        works,
-        year
+        eui.title,
+        eui.works,
+        eui.year
     INTO
-        edition_title,
-        works_keys,
-        book_year
-    FROM editions_unnested_isbns
-    WHERE isbn = isbn_param;
+        edition_title_var,
+        work_keys_var,
+        year_var
+    FROM editions_unnested_isbns eui
+    WHERE eui.isbn = isbn_param;
 
     -- If works is in result from editions_unnested_isbns, retrieve work info and earliest year
-    IF works_keys IS NOT NULL THEN
-        SELECT title, authors INTO work_title, author_keys
-        FROM works
-        WHERE key = ANY(works_keys);
+    IF work_keys_var IS NOT NULL THEN
+        SELECT w.title, w.authors INTO work_title_var, author_keys_var
+        FROM works w
+        WHERE w.key = ANY(work_keys_var);
 
         WITH year_cte AS (
-            SELECT year
-            FROM editions
-            WHERE works && works_keys
+            SELECT e.year
+            FROM editions e
+            WHERE e.works && work_keys_var
         )
-        SELECT MIN(year) INTO min_year
-        FROM year_cte;
-
-        book_year := min_year;
+        SELECT MIN(yc.year) INTO min_year_var
+        FROM year_cte yc;
     END IF;
 
     -- Retrieve author names
     RETURN QUERY
     SELECT
-        COALESCE(edition_title, t.book_title2) AS book_title,
-        t.book_year,
-        ARRAY_AGG(a.name) AS author_names
+        COALESCE(edition_title_var, t.book_title2) AS book_title,
+        ARRAY_AGG(a.name) AS author_names,
+        t.year
     FROM (
         SELECT
-            CASE WHEN work_title IS NULL THEN edition_title ELSE work_title END AS book_title2,
-            book_year,
+            CASE WHEN work_title_var IS NULL THEN edition_title_var ELSE work_title_var END AS book_title2,
+            min_year_var AS year,
             a.key
         FROM authors a
-        WHERE key = ANY(COALESCE(author_keys, '{}'::TEXT[]))
+        WHERE key = ANY(COALESCE(author_keys_var, '{}'::TEXT[]))
     ) t
-    CROSS JOIN LATERAL UNNEST(author_keys) WITH ORDINALITY AS u(key, ord)
+    CROSS JOIN LATERAL UNNEST(author_keys_var) WITH ORDINALITY AS u(key, ord)
     LEFT JOIN authors a ON a.key = u.key
-    GROUP BY t.book_title2, t.book_year;
+    GROUP BY t.book_title2, t.year;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE FUNCTION get_book_info_by_title(title_param VARCHAR(200))
 RETURNS TABLE (
-    author_names TEXT,
-    book_year CHAR(4)
+    person TEXT,
+    year CHAR(4)
 )
 AS $$
 DECLARE
-    authors_var TEXT[];
+    author_keys_var TEXT[];
     author_names_var TEXT;
     year_var CHAR(4);
 BEGIN
     WITH distinct_title_authors AS (
-    SELECT DISTINCT ON (title_query, authors)
-        authors,
-        title_query <-> LOWER(title_param) AS distance,
-        COUNT(*) OVER (PARTITION BY title_query, authors) AS dist_count
-    FROM editions
-    WHERE title_query % LOWER(title_param)
+    SELECT DISTINCT ON (e.title_query, e.authors)
+        e.authors,
+        e.title_query <-> LOWER(title_param) AS distance,
+        COUNT(*) OVER (PARTITION BY e.title_query, e.authors) AS dist_count
+    FROM editions e
+    WHERE
+        e.title_query % LOWER(title_param)
+        AND e.authors IS NOT NULL
     )
-    SELECT authors
-    INTO authors_var
-    FROM distinct_title_authors
-    ORDER BY distance, dist_count DESC
+    SELECT dta.authors
+    INTO author_keys_var
+    FROM distinct_title_authors dta
+    ORDER BY dta.distance, dta.dist_count DESC
     LIMIT 1;
 
-    SELECT MIN(year)
+    SELECT MIN(e.year)
     INTO year_var
-    FROM editions
+    FROM editions e
     WHERE
-        title_query % LOWER(title_param)
-        AND authors = authors_var
+        e.title_query % LOWER(title_param)
+        AND e.authors = author_keys_var
     GROUP BY authors;
 
     -- Query for author names
-    SELECT STRING_AGG(name, ', ')
+    SELECT STRING_AGG(a.name, ', ')
     INTO author_names_var
-    FROM authors
-    WHERE key = ANY(authors_var);
+    FROM authors a
+    WHERE a.key = ANY(author_keys_var);
 
     RETURN QUERY
     SELECT
-        author_names_var AS author_names,
-        year_var AS book_year;
+        author_names_var AS person,
+        year_var AS year;
 END;
 $$ LANGUAGE plpgsql;
