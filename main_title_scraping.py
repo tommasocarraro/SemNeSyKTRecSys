e33df50cd5994ebbe048421b0f272e9cd0d7d5bd
bmxitalia <tommasocarraro96@gmail.com>
2024-06-16 11:36:30 -0400

    books scraping ready

--- main_title_scraping.py
+++ main_title_scraping.py
@@ -86,6 +86,6 @@ def metadata_scraping(metadata: str,
 
 
 if __name__ == "__main__":
-    metadata_scraping("./data/processed/legacy/complete-filtered-metadata.json", motivation=None,
+    metadata_scraping("./data/processed/music.json", motivation=None,
                       mode="standard", save_tmp=True, batch_size=100, use_solver=False, n_cores=10,
                       batch_idx_start=0)
