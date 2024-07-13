a50f38722a66eb73494310432ab45a508adce2dc
bmxitalia <tommasocarraro96@gmail.com>
2024-07-13 11:40:46 -0400

    updated mapping after scraping API

--- main_entity_linking.py
+++ main_entity_linking.py
@@ -3,13 +3,18 @@ from nesy.paths.entity_linking.utils import linking_stats, get_ids
 
 if __name__ == "__main__":
     # todo per eccezioni uso quello originale se la regex elimina tutto
-    # entity_linker_api_query("./complete-movies.json", "./mapping-movies.json",
-    #                         n_cores=1, retry_reason="not-in-correct-category")
-    # entity_linker_api_query("./data/processed/complete-books.json", "./data/processed/mapping-books.json",
-    #                         n_cores=1, retry_reason="long-title-exception")
-    linking_stats("./data/processed/mapping-movies.json", errors=["not-title", "not-found-query",
-                                                                  "exception", "not-in-correct-category", "long-title-exception"])
-    linking_stats("./data/processed/mapping-music.json", errors=["not-title", "not-found-query",
-                                                                 "exception", "not-in-correct-category", "long-title-exception"])
-    linking_stats("./data/processed/mapping-books.json", errors=["not-title", "not-found-query",
-                                                                 "exception", "not-in-correct-category", "long-title-exception"])
+    entity_linker_api_query("./data/processed/augmented-movies.json", "./data/processed/mapping-aug-movies.json",
+                            n_cores=10)
+    entity_linker_api_query("./data/processed/augmented-music.json", "./data/processed/mapping-aug-music.json",
+                            n_cores=10)
+    entity_linker_api_query("./data/processed/augmented-books.json", "./data/processed/mapping-aug-books.json",
+                            n_cores=10)
+    linking_stats("./data/processed/mapping-aug-movies.json", errors=["not-title", "not-found-query",
+                                                                      "exception", "not-in-correct-category",
+                                                                      "long-title-exception"])
+    linking_stats("./data/processed/mapping-aug-music.json", errors=["not-title", "not-found-query",
+                                                                     "exception", "not-in-correct-category",
+                                                                     "long-title-exception"])
+    linking_stats("./data/processed/mapping-aug-books.json", errors=["not-title", "not-found-query",
+                                                                     "exception", "not-in-correct-category",
+                                                                     "long-title-exception"])
