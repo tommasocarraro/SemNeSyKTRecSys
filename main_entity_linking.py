b75da386f4a5f6f30dcea3fcf6bbca1e2e4fe811
bmxitalia <tommasocarraro96@gmail.com>
2024-07-20 01:33:00 -0400

    loaded missing file
    Former-commit-id: 97fac2910178ef5584b4e4299aa904fde9d40ac2

--- main_entity_linking.py
+++ main_entity_linking.py
@@ -2,19 +2,18 @@ from nesy.paths.entity_linking.wiki_special_search_linker import entity_linker_a
 from nesy.paths.entity_linking.utils import linking_stats, get_ids
 
 if __name__ == "__main__":
-    # todo per eccezioni uso quello originale se la regex elimina tutto
-    entity_linker_api_query("./data/processed/augmented-movies.json", "./data/processed/mapping-aug-movies.json",
-                            n_cores=10)
-    entity_linker_api_query("./data/processed/augmented-music.json", "./data/processed/mapping-aug-music.json",
-                            n_cores=10)
-    entity_linker_api_query("./data/processed/augmented-books.json", "./data/processed/mapping-aug-books.json",
-                            n_cores=10)
-    linking_stats("./data/processed/mapping-aug-movies.json", errors=["not-title", "not-found-query",
+    # entity_linker_api_query("./data/processed/augmented-movies.json", "./data/processed/mapping-aug-movies.json",
+    #                         n_cores=10)
+    # entity_linker_api_query("./data/processed/augmented-music.json", "./data/processed/mapping-aug-music.json",
+    #                         n_cores=10)
+    # entity_linker_api_query("./data/processed/augmented-books.json", "./data/processed/mapping-aug-books.json",
+    #                         n_cores=10)
+    linking_stats("./data/processed/mappings/mapping-movies.json", errors=["not-title", "not-found-query",
                                                                       "exception", "not-in-correct-category",
                                                                       "long-title-exception"])
-    linking_stats("./data/processed/mapping-aug-music.json", errors=["not-title", "not-found-query",
+    linking_stats("./data/processed/mappings/mapping-music.json", errors=["not-title", "not-found-query",
                                                                      "exception", "not-in-correct-category",
                                                                      "long-title-exception"])
-    linking_stats("./data/processed/mapping-aug-books.json", errors=["not-title", "not-found-query",
+    linking_stats("./data/processed/mappings/mapping-books.json", errors=["not-title", "not-found-query",
                                                                      "exception", "not-in-correct-category",
                                                                      "long-title-exception"])
