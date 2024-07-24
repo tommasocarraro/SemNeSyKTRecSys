f9469a172505e60075bf83159f796dbf8332490a
bmxitalia <tommasocarraro96@gmail.com>
2024-07-24 15:25:38 -0400

    removed unwanted properties from DB
    Former-commit-id: 700e1251f6c0972fd0e5dea0532144421af2e533

--- main_path_finding.py
+++ main_path_finding.py
@@ -4,6 +4,9 @@ if __name__ == '__main__':
     neo4j_path_finder("data/processed/mappings/mapping-movies.json",
                       "data/processed/mappings/mapping-music.json",
                       "./data/processed/paths/path-movies-music.json",
-                      max_hops=10, n_cores=120)
-    # todo vedere se il discorso di pre-caricare il grafo puo' aiutare
-    # todo bisogna per forza rendere piu' efficienti le cose gestendo la dimensione del batch
+                      max_hops=10, n_cores=10)
+    # todo adesso siamo rimasti che bisogna capire perche' si ferma e non e' lineare l'andamento
+    # todo vedere discorso memoria con consigli su sito Neo4j -> fatto
+    # todo eliminare piu' relazioni e nodi possibili -> fatto
+    # todo caricare in cache con una query stupida
+    # todo piano B -> distribuzioni dataset oppure eliminare cio' che abbiamo matchato con solo titolo
