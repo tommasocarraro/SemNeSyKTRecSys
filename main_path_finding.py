a08b24b167c45a741be8ddce4eff13273b7df83c
bmxitalia <tommasocarraro96@gmail.com>
2024-07-26 12:08:49 -0400

    ready for test on server

--- main_path_finding.py
+++ main_path_finding.py
@@ -4,6 +4,6 @@ from nesy.paths.neo4j.utils import get_cold_start, get_rating_stats, refine_cold
 if __name__ == '__main__':
     neo4j_path_finder("data/processed/mappings/mapping-movies.json",
                       "data/processed/mappings/mapping-music.json",
-                      max_hops=10, n_cores=10, cold_start=True, popular=True)
+                      max_hops=10, n_cores=10, cold_start=True, popular=True, cs_threshold=5, pop_threshold=300)
     # todo piano B -> distribuzioni dataset oppure eliminare cio' che abbiamo matchato con solo titolo
     # todo -> vedo i cold-start user. per ogni user, vedo gli item -> questa e' una buona opzione per il cold-start user
