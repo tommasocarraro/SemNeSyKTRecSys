b39c1e9e2073f3df54c609f75c5f3e141c043375
bmxitalia <tommasocarraro96@gmail.com>
2024-07-29 12:14:46 -0400

    ready for server
    Former-commit-id: 650a478a9292fdcb233c367097dfbc38ced1a91c

--- main_path_finding.py
+++ main_path_finding.py
@@ -3,4 +3,4 @@ from nesy.paths.neo4j.path_finder import neo4j_path_finder
 if __name__ == '__main__':
     neo4j_path_finder("data/processed/mappings/mapping-movies.json",
                       "data/processed/mappings/mapping-music.json",
-                      max_hops=10, n_cores=10, cold_start=True, popular=True, cs_threshold=5, pop_threshold=300)
+                      max_hops=10, n_cores=120, cold_start=True, popular=True, cs_threshold=5, pop_threshold=200)
