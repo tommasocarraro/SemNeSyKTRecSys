6e3df5e1753946f4cbba1cb087d8d04d602958ac
bmxitalia <tommasocarraro96@gmail.com>
2024-07-29 16:42:44 -0400

    ready for generating paths
    Former-commit-id: 0ff54e880f599757966450db9507a54b2b06d58c

--- codicefurbo.py
+++
@@ -1,5 +0,0 @@
-from nesy.utils import get_mapping_stats
-
-print(get_mapping_stats("./data/processed/mappings/mapping-movies.json"))  # 11'517
-print(get_mapping_stats("./data/processed/mappings/mapping-music.json"))  # 40'276
-print(get_mapping_stats("./data/processed/mappings/mapping-books.json"))  # 26'220
