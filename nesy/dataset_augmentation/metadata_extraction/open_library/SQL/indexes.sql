d6148caab4d6e913ab9a59ec9301fa1a553e0ad8
bmxitalia <tommasocarraro96@gmail.com>
2024-10-24 11:32:22 +0200

    new main branch
    Former-commit-id: 1208be295c2bf1a95c4a5723225efe3de316930b

--- nesy/dataset_augmentation/metadata_extraction/open_library/SQL/indexes.sql
+++
@@ -1,8 +0,0 @@
-CREATE INDEX ON editions USING GIN (title_query gin_trgm_ops);
-CREATE INDEX ON editions USING GIN (works);
-
-CREATE INDEX ON editions_authors USING GIN (name_query gin_trgm_ops);
-CREATE INDEX ON editions_authors USING GIN (title_query gin_trgm_ops);
-CREATE INDEX ON editions_authors (year);
-
-CREATE INDEX ON editions_unnested_isbns (isbn);
