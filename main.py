f67b12d3a6013ba139c1e6d9206c75e598ef9977
bmxitalia <tommasocarraro96@gmail.com>
2024-10-29 18:00:27 +0100

    fixed train_test_split

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from src.data import process_source_target
+from src.loader import DataLoader
+from src.models.mf import MFTrainer, MatrixFactorization
+import torch
 
 process = process_source_target(0, "./data/ratings/reviews_Books_5.csv",
                                 "./data/ratings/reviews_Movies_and_TV_5.csv",
                                 "./data/kg_paths/books(pop:300)-movies(cs:5).json")
+
+tr_loader = DataLoader(process["src_tr"], 128)
+val_loader = DataLoader(process["src_val"], 128)
+
+mf = MatrixFactorization(process["src_n_users"], process["src_n_items"], 10)
+
+tr = MFTrainer(mf, torch.optim.Adam(mf.parameters(), lr=1e-3), torch.nn.MSELoss())
+
+tr.train(tr_loader, val_loader, "mse")
