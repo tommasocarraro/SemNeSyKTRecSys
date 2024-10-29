64d7a89d4b48eefe9c83f8beb852a572987d69ed
Nicolò Bertocco <nick.bertocco@gmail.com>
2024-10-29 09:16:11 +0100

    optimized test ids sampling
    Former-commit-id: e2ec4f6c1c55e831751b7be2fa5054356634a2fd

--- src/data.py
+++ src/data.py
@@ -1,29 +1,40 @@
 import json
 import pandas as pd
+from pandas import DataFrame
 from sklearn.model_selection import train_test_split as train_test_split_sklearn
 import numpy as np
 from scipy.sparse import csr_array
 import torch
+from joblib import Parallel, delayed
+from tqdm import tqdm
 
 
-def train_test_split(seed, ratings, frac=None, user_level=False):
+def train_test_split(
+    seed: float, ratings: DataFrame, frac: float = None, user_level=False
+):
     """
     It splits the dataset into training and test sets.
 
     :param seed: seed for random reproducibility
     :param ratings: dataframe containing the dataset ratings that have to be split
-    :param frac: proportion of ratings to be sampled to create the test set. If None, it randomly sample one positive
+    :param frac: proportion of ratings to be sampled to create the test set. If None, it randomly samples one positive
     rating (LOO)
     :param user_level: whether the train test split has to be performed at the user level or ratings can
-    be sampled randomly independently from the user. Defaults to False, meaning that the split is not done at the
+    be sampled randomly independently of the user. Defaults to False, meaning that the split is not done at the
     user level. In case the split is not done at the user level, a stratified split is performed to mantain the
     distribution of the classes
     :return: train and test set dataframes
     """
     if user_level:
-        test_ids = ratings.groupby(by=["userId"]).apply(
-            lambda x: x.sample(frac=frac, random_state=seed)
+        ratings_by_user = ratings.groupby(by=["userId"])
+
+        test_ids = Parallel(n_jobs=-1, backend="loky")(
+            delayed(lambda user: user.sample(frac=frac, random_state=seed))(user)
+            for _, user in tqdm(
+                ratings_by_user, desc="Sampling test IDs...", dynamic_ncols=True
+            )
         )
+
         test_ids = test_ids.index.get_level_values(1)
         train_ids = np.setdiff1d(ratings.index.values, test_ids)
         train_set = ratings.iloc[train_ids].reset_index(drop=True)
@@ -31,13 +42,24 @@ def train_test_split(seed, ratings, frac=None, user_level=False):
         return train_set, test_set
     else:
         # we use scikit-learn train-test split
-        return train_test_split_sklearn(ratings, random_state=seed, stratify=ratings["sentiment"],
-                                        test_size=frac)
+        # TODO sentiment is not a column in this dataset
+        return train_test_split_sklearn(
+            ratings, random_state=seed, stratify=ratings["sentiment"], test_size=frac
+        )
 
 
-def process_source_target(seed, source, target, paths, implicit=True, source_val_size=0.2,
-                          source_user_level_split=True,
-                          target_val_size=0.1, target_test_size=0.2, target_user_level_split=True):
+def process_source_target(
+    seed: float,
+    source_dataset_path: str,
+    target_dataset_path: str,
+    paths_file_path: str,
+    implicit=True,
+    source_val_size=0.2,
+    source_user_level_split=True,
+    target_val_size=0.1,
+    target_test_size=0.2,
+    target_user_level_split=True,
+):
     """
     This function processes the cross-domain dataset and prepares it for the experiment. In particular, it executes
     the following pipeline:
@@ -47,9 +69,9 @@ def process_source_target(seed, source, target, paths, implicit=True, source_val
     is semantically connected (there exists a KG paths) to the target domain item j, 0 otherwise
 
     :param seed: seed for reproducibility
-    :param source: path to the source dataset
-    :param target: path to the target dataset
-    :param paths: paths between entities in the two different domain
+    :param source_dataset_path: path to the source dataset
+    :param target_dataset_path: path to the target dataset
+    :param paths_file_path: paths between entities in the two different domain
     :param implicit: whether to convert explicit ratings to implicit feedback
     :param source_val_size: size of validation set for source domain dataset
     :param source_user_level_split: whether the split for the source dataset has to be done at the user level or not
@@ -58,13 +80,20 @@ def process_source_target(seed, source, target, paths, implicit=True, source_val
     :param target_user_level_split: whether the split for the target dataset has to be done at the user level or not
     """
     # get source and target ratings
-    src_ratings = pd.read_csv(source, usecols=["userId", "itemId", "rating"])
-    tgt_ratings = pd.read_csv(target, usecols=["userId", "itemId", "rating"])
+    src_ratings = pd.read_csv(
+        source_dataset_path, usecols=["userId", "itemId", "rating"]
+    )
+    tgt_ratings = pd.read_csv(
+        target_dataset_path, usecols=["userId", "itemId", "rating"]
+    )
+
     # get shared users
     sh_users = set(src_ratings["userId"]) & set(tgt_ratings["userId"])
+
     # get ids of shared users
     sh_u_ids = list(range(len(sh_users)))
     sh_users_string_to_id = dict(zip(sh_users, sh_u_ids))
+
     # get ids of non-shared users in source and target domains
     src_users = set(src_ratings["userId"]) - sh_users
     src_u_ids = [(i + len(sh_u_ids)) for i in range(len(src_users))]
@@ -74,6 +103,7 @@ def process_source_target(seed, source, target, paths, implicit=True, source_val
     tgt_u_ids = [(i + len(sh_u_ids)) for i in range(len(tgt_users))]
     tgt_u_string_to_id = dict(zip(tgt_users, tgt_u_ids))
     tgt_u_string_to_id.update(sh_users_string_to_id)
+
     # get ids of items in source and target domain
     src_i_ids = src_ratings["itemId"].unique()
     int_src_i_ids = list(range(len(src_i_ids)))
@@ -81,6 +111,7 @@ def process_source_target(seed, source, target, paths, implicit=True, source_val
     tgt_i_ids = tgt_ratings["itemId"].unique()
     int_tgt_i_ids = list(range(len(tgt_i_ids)))
     tgt_i_string_to_id = dict(zip(tgt_i_ids, int_tgt_i_ids))
+
     # apply the new indexing
     src_ratings["userId"] = src_ratings["userId"].map(src_u_string_to_id)
     src_ratings["itemId"] = src_ratings["itemId"].map(src_i_string_to_id)
@@ -91,28 +122,56 @@ def process_source_target(seed, source, target, paths, implicit=True, source_val
         # convert ratings to implicit feedback
         src_ratings["rating"] = (src_ratings["rating"] >= 4).astype(int)
         tgt_ratings["rating"] = (tgt_ratings["rating"] >= 4).astype(int)
+
     # get number of users and items in source and target domains
     src_n_users = src_ratings["userId"].nunique()
     src_n_items = src_ratings["itemId"].nunique()
     tgt_n_users = tgt_ratings["userId"].nunique()
     tgt_n_items = tgt_ratings["itemId"].nunique()
+
     # create train and validation set for source domain dataset
-    src_tr, src_val = train_test_split(seed, src_ratings, frac=source_val_size, user_level=source_user_level_split)
+    src_tr, src_val = train_test_split(
+        seed, src_ratings, frac=source_val_size, user_level=source_user_level_split
+    )
+
     # create train, validation and test set for target domain dataset
-    tgt_tr, tgt_te = train_test_split(seed, tgt_ratings, frac=target_test_size, user_level=target_user_level_split)
-    tgt_tr_small, tgt_val = train_test_split(seed, tgt_tr, frac=target_val_size, user_level=target_user_level_split)
+    tgt_tr, tgt_te = train_test_split(
+        seed, tgt_ratings, frac=target_test_size, user_level=target_user_level_split
+    )
+    tgt_tr_small, tgt_val = train_test_split(
+        seed, tgt_tr, frac=target_val_size, user_level=target_user_level_split
+    )
 
     # create source_items X target_items matrix (used for the Sim predicate in the model)
-    with open(paths, "r") as json_paths:
-        paths = json.load(json_paths)
-    available_path_pairs = np.array([(src_asin, tgt_asin) for src_asin, tgt_asins in paths.items()
-                                     for tgt_asin, _ in tgt_asins.items()])
-    # create sparse sim matrix
-    sim_matrix = csr_array((np.ones(available_path_pairs.shape[0]),
-                            (available_path_pairs[:, 0], available_path_pairs[:, 1])),
-                           shape=(src_n_items, tgt_n_items))
+    with open(paths_file_path, "r") as json_paths:
+        paths_file_path = json.load(json_paths)
+    available_path_pairs = np.array(
+        [
+            (src_asin, tgt_asin)
+            for src_asin, tgt_asins in paths_file_path.items()
+            for tgt_asin, _ in tgt_asins.items()
+        ]
+    )
 
-    return {"src_n_users": src_n_users, "src_n_items": src_n_items, "tgt_n_users": tgt_n_users,
-            "tgt_n_items": tgt_n_items, "src_tr": src_tr.to_numpy(), "src_val": src_val.to_numpy(),
-            "tgt_tr": tgt_tr.to_numpy(), "tgt_tr_small": tgt_tr_small.to_numpy(), "tgt_val": tgt_val.to_numpy(),
-            "tgt_te": tgt_te.to_numpy(), "sim_matrix": torch.tensor(sim_matrix.toarray())}
+    # create sparse sim matrix
+    sim_matrix = csr_array(
+        (
+            np.ones(available_path_pairs.shape[0]),
+            (available_path_pairs[:, 0], available_path_pairs[:, 1]),
+        ),
+        shape=(src_n_items, tgt_n_items),
+    )
+
+    return {
+        "src_n_users": src_n_users,
+        "src_n_items": src_n_items,
+        "tgt_n_users": tgt_n_users,
+        "tgt_n_items": tgt_n_items,
+        "src_tr": src_tr.to_numpy(),
+        "src_val": src_val.to_numpy(),
+        "tgt_tr": tgt_tr.to_numpy(),
+        "tgt_tr_small": tgt_tr_small.to_numpy(),
+        "tgt_val": tgt_val.to_numpy(),
+        "tgt_te": tgt_te.to_numpy(),
+        "sim_matrix": torch.tensor(sim_matrix.toarray()),
+    }
