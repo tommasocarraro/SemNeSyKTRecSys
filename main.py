0c0e755f89517bb5f74e3d4e564d842b74095617
Nicolò Bertocco <nick.bertocco@gmail.com>
2024-11-05 17:04:13 +0100

    switched to mfclassifier, bce loss and fbeta-1 val metric
    Former-commit-id: 97b5ab6e316130be7a944f9cefea90e72c1d92aa

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.data import process_source_target
 from src.loader import DataLoader
-from src.models.mf import MFTrainer, MatrixFactorization
+from src.models.mf import MFTrainerClassifier, MatrixFactorization
 import torch
 
 process = process_source_target(0, "./data/ratings/reviews_Books_5.csv",
@@ -8,11 +8,11 @@ process = process_source_target(0, "./data/ratings/reviews_Books_5.csv",
                                 "./data/kg_paths/books(pop:300)-movies(cs:5).json",
                                 save_path="./data/saved_data/dataset.npy")
 
-tr_loader = DataLoader(process["src_tr"], 256)
+tr_loader = DataLoader(process["src_tr"], 512)
 val_loader = DataLoader(process["src_val"], 256)
 
 mf = MatrixFactorization(process["src_n_users"], process["src_n_items"], 10)
 
-tr = MFTrainer(mf, torch.optim.Adam(mf.parameters(), lr=1e-3), torch.nn.MSELoss())
+tr = MFTrainerClassifier(mf, torch.optim.Adam(mf.parameters(), lr=1e-3), torch.nn.BCEWithLogitsLoss())
 
-tr.train(tr_loader, val_loader, "mse", verbose=1)
+tr.train(tr_loader, val_loader, "fbeta-1", verbose=1)
