bug fix on AUC computation
Former-commit-id: 36e589dc5750d241dbc8c613d8771c88154498df

main.py
@@ -4,6 +4,7 @@ from src.bpr_loss import BPRLoss
 from src.data import process_source_target
 from src.loader import DataLoader
 from src.models.mf import MFTrainer, MatrixFactorization
+from src.utils import set_seed
 
 process = process_source_target(
     0,
@@ -13,13 +14,18 @@ process = process_source_target(
     save_path="./data/saved_data/",
 )
 
+set_seed(0)
+
 tr_loader = DataLoader(process["src_tr"], process["src_n_items"], 512)
 val_loader = DataLoader(process["src_val"], process["src_n_items"], 512)
 
-mf = MatrixFactorization(process["src_n_users"], process["src_n_items"], 10)
+mf = MatrixFactorization(process["src_n_users"], process["src_n_items"], 5)
 
 tr = MFTrainer(
-    mf, torch.optim.AdamW(mf.parameters(), lr=0.0001, weight_decay=0.001), BPRLoss()
+    mf, torch.optim.AdamW(mf.parameters(), lr=0.0001, weight_decay=0.0001), BPRLoss()
 )
 
 tr.train(tr_loader, val_loader, "auc", early=10, verbose=1)
+
+# TODO exact sampling of negative without the risk of sampling positives
+# TODO early stopping with torch.isnan because we need to stop if we detect exploding or vanishing gradients

src/metrics.py
@@ -81,26 +81,37 @@ def check_metrics(metrics: Union[str, list[str]]):
     ), err_msg
 
 
-def auc(pos_preds: NDArray, neg_preds: NDArray):
-    # TODO the average here is general, is it maybe better to do it at the user level and then average again?
+def auc(users: NDArray, pos_preds: NDArray, neg_preds: NDArray):
     """
     Computes the AUC score between predicted and target ratings. The given scores are aligned, namely the first positive
     score has to be compared with the first negative score. In other words, they correspond to the same user.
 
+    :param users: user indexes for user-level mean
     :param pos_preds: scores for positive interactions
     :param neg_preds: scores for negative interactions
     :return: average AUC score across all the validation set
     """
-    return np.mean((pos_preds - neg_preds) > 0)
+    single_values = (pos_preds - neg_preds) > 0
+    # get unique users and their indices
+    unique_users, inverse_indices = np.unique(users, return_inverse=True)
+    # group single_values by user
+    user_sums = np.bincount(inverse_indices, weights=single_values, minlength=unique_users.size)
+    user_counts = np.bincount(inverse_indices, minlength=unique_users.size)
+    # compute user-level means
+    user_means = user_sums / user_counts
+    # final mean across all users
+    final_mean_auc = user_means.mean()
+    return final_mean_auc
 
 
-def compute_metric(metric: str, pred_scores: NDArray, ground_truth: NDArray = None):
+def compute_metric(metric: str, pred_scores: NDArray, ground_truth: NDArray = None, users: NDArray = None):
     """
     Compute the given metric on the given predictions and ground truth.
 
     :param metric: name of the metric that has to be computed
     :param pred_scores: predicted scores for validation user-item pairs
     :param ground_truth: target ratings for validation user-item pairs
+    :param users: user indexes. Optional. At the moment, only used on AUC computation.
     :return: the value of the given metric for the given predictions and ground truth
     """
     if "-" in metric:
@@ -113,4 +124,4 @@ def compute_metric(metric: str, pred_scores: NDArray, ground_truth: NDArray = No
         elif metric == "acc":
             return acc(pred_scores, ground_truth)
         elif metric == "auc":
-            return auc(pred_scores, ground_truth)
+            return auc(users, pred_scores, ground_truth)

src/trainer.py
@@ -91,6 +91,7 @@ class Trainer:
                     # log training information
                     wandb.log(log_dict)
             # save best model and update early stop counter, if necessary
+            # TODO stop even if there is NaN on the train loss
             if val_score > best_val_score:
                 best_val_score = val_score
                 if self.wandb_train:
@@ -138,11 +139,12 @@ class Trainer:
         :param loader: loader containing evaluation data
         :return: predictions and targets
         """
-        pos_preds, neg_preds = [], []
+        users_, pos_preds, neg_preds = [], [], []
         for batch_idx, (users, pos_items, neg_items) in enumerate(loader):
+            users_.append(users.cpu().numpy())
             pos_preds.append(self.predict(users, pos_items).cpu().numpy())
             neg_preds.append(self.predict(users, neg_items).cpu().numpy())
-        return np.concatenate(pos_preds), np.concatenate(neg_preds)
+        return np.concatenate(users_), np.concatenate(pos_preds), np.concatenate(neg_preds)
 
     def compute_validation_loss(self, pos_preds, neg_preds):
         """
@@ -164,9 +166,9 @@ class Trainer:
         :return: validation score based on the given metric averaged across all validation examples
         """
         # prepare predictions and targets for evaluation
-        pos_preds, neg_preds = self.prepare_for_evaluation(val_loader)
+        users, pos_preds, neg_preds = self.prepare_for_evaluation(val_loader)
         # compute validation metric
-        val_score = compute_metric(val_metric, pos_preds, neg_preds)
+        val_score = compute_metric(val_metric, pos_preds, neg_preds, users)
         # compute validation loss
         validation_loss = None
         if val_loss: