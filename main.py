254017ae1a1a1226718a32318df20800a587725b
bmxitalia <tommasocarraro96@gmail.com>
2024-12-08 17:38:57 +0100

    now the negative sampling does not include positive items

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import torch
 
 from src.bpr_loss import BPRLoss
 from src.data import process_source_target
-from src.loader import DataLoader
+from src.loader import DataLoader, DataLoaderSamuel
 from src.models.mf import MFTrainer, MatrixFactorization
 from src.utils import set_seed
 
@@ -16,15 +16,13 @@ process = process_source_target(
 
 set_seed(0)
 
-tr_loader = DataLoader(process["src_tr"], process["src_ui_matrix"], 512)
+tr_loader = DataLoaderSamuel(process["src_tr"], process["src_ui_matrix"], 512)
 val_loader = DataLoader(process["src_val"], process["src_ui_matrix"], 512)
 
 mf = MatrixFactorization(process["src_n_users"], process["src_n_items"], 5)
 
 tr = MFTrainer(
-    mf, torch.optim.AdamW(mf.parameters(), lr=0.1, weight_decay=0.00001), BPRLoss()
+    mf, torch.optim.AdamW(mf.parameters(), lr=0.0001, weight_decay=0.0001), BPRLoss()
 )
 
 tr.train(tr_loader, val_loader, "auc", early=10, verbose=1)
-
-# TODO exact sampling of negative without the risk of sampling positives
