fixes on AUC computation
Former-commit-id: cc78a68f5b778891385cbe11912b61a7b30854e1

main.py
@@ -22,7 +22,7 @@ val_loader = DataLoader(process["src_val"], process["src_ui_matrix"], 512)
 mf = MatrixFactorization(process["src_n_users"], process["src_n_items"], 5)
 
 tr = MFTrainer(
-    mf, torch.optim.AdamW(mf.parameters(), lr=0.0001, weight_decay=0.0001), BPRLoss()
+    mf, torch.optim.AdamW(mf.parameters(), lr=0.01, weight_decay=0.0001), BPRLoss()
 )
 
 tr.train(tr_loader, val_loader, "auc", early=10, verbose=1)