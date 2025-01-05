42c24ca2273b0c5e01ccad547cde4c1349d75e5c
bmxitalia <tommasocarraro96@gmail.com>
2025-01-05 14:14:34 +0100

    finished implementation of inference and checked utils

--- src/source_pretrain/inference.py
+++ src/source_pretrain/inference.py
@@ -8,34 +8,19 @@ from src.device import device
 
 
 def generate_pre_trained_src_matrix(
-    mf_model, best_weights, n_shared_users, src_ui_matrix, pos_threshold, batch_size
+    mf_model, best_weights, n_shared_users, k, batch_size
 ) -> csr_matrix:
     """
-    Generates the dense source domain user-item matrix filled with predictions from a model pre-trained on the source
-    domain. This will be the implementation of the LikesSource predicate in the LTN model.
-
-    Note that the matrix will be n_shared_users x n_items because we can transfer knowledge just for shared users. The
-    indexes from 0 to n_shared_users - 1 are the indexes of the shared users. So, in row 0, there will be predictions
-    of the pre-trained model on the source domain for the user 0, that is the first shared user. Note that in the
-    original indexing, the first n_shared_users indexes were dedicated to the shared users, so it is enough to compute
-    predictions for this portion of users.
-
-    Since the model on the source domain is trained with a BPR criterion (ranking) and we need 0/1 values, we use the
-    following heuristic to convert rankings into desired values:
-    1. For each user, we create the ranking on the entire catalog of the source domain;
-    2. For each ranking, we take the first pos_threshold items and set them as positives in the final sparse matrix.
+    This function takes the pre-trained MF model in the source domain and generates a ranking of source domain items for
+    each shared user. The first k position in the ranking are the top recommended items
+    that are then used by the LTN model to transfer knowledge.
 
     :param mf_model: architecture of the Matrix Factorization model whose best weights have to be loaded
     :param best_weights: path to the best weights that have to be loaded in the Matrix Factorization architecture
     :param n_shared_users: number of shared users across domains
-    :param src_ui_matrix: source domain user-item matrix
-    :param pos_threshold: threshold that determines from which ranking position items has to be predicted as negatives.
-    All the items in the top pos_threshold positions will be classified as positives. All the items in ranking positions
-    lower than pos_threshold will be classified as negatives.
+    :param k: number of items at the top of the ranking that have to be treated as positive items.
     :param batch_size: number of predictions to be computed in parallel at each prediction step.
     """
-    # TODO mettere cold start user tutto a zero sulla matrice LikesSource, cosi non trasferisce nulla
-    # TODO mettere i rating della user-item matrix a uno sugli scores o comunque fare in modo che siano gli scores piu' alti
     # load the best weights on the model
     mf_model.load_state_dict(torch.load(best_weights, map_location=device))
     # initialize predictions tensor
@@ -49,95 +34,9 @@ def generate_pre_trained_src_matrix(
             items = torch.arange(start_idx, end_idx, dtype=torch.long, device=device)
             with torch.no_grad():
                 preds[u, start_idx:end_idx] = mf_model(users, items)
-        user_0_preds = preds[0]
-        pass
 
-    # create the rankings for each user and take the indexes of the items in the first pos_threshold positions
-    # these will be the items that have to have a 1 in the LikesSource matrix
-    pos_idx = torch.argsort(preds, dim=1, descending=True)[:, :pos_threshold].flatten().numpy()
-
-    # create dense matrix with predictions where we put 1 if the item has a score that, once the softmax is applied,
-    # is near 1, and 0 otherwise
-
-    # take portion of user-item matrix dedicated to shared users
-    sh_users_src_ui_matrix = src_ui_matrix[:n_shared_users]
-    rows, cols = sh_users_src_ui_matrix.nonzero()
-    ui_matrix_coords = set(zip(rows, cols))
-    # compute the coordinates (rows, cols) of the new positions that have to be put at 1 (there are the predictions
-    # of the pre-trained model)
-    new_rows = np.arange(0, n_shared_users).repeat(pos_threshold)
-    new_coords = set(zip(new_rows, pos_idx))
-    # build final user-item matrix for the LikesSource predicate where there is a 1 if the pre-trained model predicted
-    # the ui pair to be in a high rank or if there was a 1 originally in the dataset
-    final_coords = new_coords | ui_matrix_coords
-    rows, cols = zip(*final_coords)
-    final_matrix = csr_matrix(([1] * len(final_coords), (rows, cols)), sh_users_src_ui_matrix.shape)
-
-    return final_matrix
-
-
-def generate_pre_trained_src_matrix_2(
-    mf_model, best_weights, n_shared_users, src_ui_matrix, beta, batch_size
-) -> csr_matrix:
-    """
-    Generates the dense source domain user-item matrix filled with predictions from a model pre-trained on the source
-    domain. This will be the implementation of the LikesSource predicate in the LTN model.
-
-    Note that the matrix will be n_shared_users x n_items because we can transfer knowledge just for shared users. The
-    indexes from 0 to n_shared_users - 1 are the indexes of the shared users. So, in row 0, there will be predictions
-    of the pre-trained model on the source domain for the user 0, that is the first shared user. Note that in the
-    original indexing, the first n_shared_users indexes were dedicated to the shared users, so it is enough to compute
-    predictions for this portion of users.
-
-    Since the model on the source domain is trained with a BPR criterion (ranking) and we need 0/1 values, we use the
-    following heuristic to convert rankings into desired values:
-    1. For each user, we create the ranking on the entire catalog of the source domain;
-    2. We take the beta softmax of each ranking so that these are mapped to values near 0 and 1;
-    3. Values near 0. will be converted to 0., while values near 1. will be converted to 1.
-
-    :param mf_model: architecture of the Matrix Factorization model whose best weights have to be loaded
-    :param best_weights: path to the best weights that have to be loaded in the Matrix Factorization architecture
-    :param n_shared_users: number of shared users across domains
-    :param src_ui_matrix: source domain user-item matrix
-    :param beta: hyper-parameter to be used for the softmax function that has to be applied to the ranking.
-    :param batch_size: number of predictions to be computed in parallel at each prediction step.
-    """
-    # TODO mettere cold start user tutto a zero sulla matrice LikesSource, cosi non trasferisce nulla
-    # load the best weights on the model
-    mf_model.load_state_dict(torch.load(best_weights, map_location=device))
-    mf_model.n_items = 800
-    # initialize predictions tensor
-    preds = torch.zeros((n_shared_users, mf_model.n_items))
-    # compute predictions for all shared users and items pairs and put them in the preds tensor
-    # each row is a user and contains the predictions for all the items in the catalog for that user
-    for u in tqdm(range(n_shared_users)):
-        for start_idx in range(0, mf_model.n_items, batch_size):
-            end_idx = min(start_idx + batch_size, mf_model.n_items)
-            users = torch.full((end_idx - start_idx,), u, dtype=torch.long, device=device)
-            items = torch.arange(start_idx, end_idx, dtype=torch.long, device=device)
-            with torch.no_grad():
-                preds[u, start_idx:end_idx] = mf_model(users, items)
-
-    # apply the softmax function with Beta to scale the predicted scores to 1/0
-    scaled_scores = softmax(beta * preds, dim=1)
-
-    # create dense matrix with predictions where we put 1 if the item has a score that, once the softmax is applied,
-    # is near 1, and 0 otherwise
-
-    # take portion of user-item matrix dedicated to shared users
-    sh_users_src_ui_matrix = src_ui_matrix[:n_shared_users]
-    rows, cols = sh_users_src_ui_matrix.nonzero()
-    ui_matrix_coords = set(zip(rows, cols))
-    # compute the coordinates (rows, cols) of the new positions that have to be put at 1 (there are the predictions
-    # of the pre-trained model)
-    # new_rows = np.arange(0, n_shared_users).repeat(pos_threshold)
-    # new_coords = set(zip(new_rows, pos_idx))
-    # # build final user-item matrix for the LikesSource predicate where there is a 1 if the pre-trained model predicted
-    # # the ui pair to be in a high rank or if there was a 1 originally in the dataset
-    # final_coords = new_coords | ui_matrix_coords
-    # rows, cols = zip(*final_coords)
-    # final_matrix = csr_matrix(
-    #     ([1] * len(final_coords), (rows, cols)), sh_users_src_ui_matrix.shape
-    # )
-    #
-    # return final_matrix
+    # create the rankings for each user and take the indexes of the items in the first k positions
+    # these will be the items for which the model is more certain and that will be used for transferring knowledge to
+    # the target domain
+    pos_items = torch.argsort(preds, dim=1, descending=True)[:, :k].numpy()
+    return pos_items
--- src/target/utils.py
+++ src/target/utils.py
@@ -3,20 +3,24 @@ from scipy.sparse import csr_matrix
 from tqdm import tqdm
 
 
-def get_reg_axiom_data(tgt_ui_matrix: csr_matrix,
+def get_reg_axiom_data(src_ui_matrix: csr_matrix,
+                       tgt_ui_matrix: csr_matrix,
                        n_sh_users: int,
                        sim_matrix: csr_matrix,
-                       top_k_items: dict[int, list[int]]):
+                       top_k_items: np.ndarray):
     """
     This function generates user-item pairs that will be given in input to the second axiom of the LTN model, namely the
     axiom that perform logical regularization based on information transferred from the source domain to the target
     domain.
 
+    :param src_ui_matrix: sparse user-item matrix containing positive interactions in the source domain. It is used to
+    determine the cold-start users in the source domain. Knowledge cannot be transferred from these users as the model
+    learned few information.
     :param tgt_ui_matrix: sparse user-item matrix containing positive interactions in the target domain
     :param n_sh_users: number of shared users across domains
     :param sim_matrix: similarity matrix containing a 1 if there exists a path between the source and target item,
     0 otherwise
-    :param top_k_items: dictionary containing for each shared users a list of top-k items the user might like, generated
+    :param top_k_items: numpy array containing for each shared user a list of top-k items the user might like, generated
     using a recommendation model pre-trained in the source domain
     """
     # find IDs of source domain items for which there exists a path with at least one target domain item
@@ -26,37 +30,43 @@ def get_reg_axiom_data(tgt_ui_matrix: csr_matrix,
     # iterate through each user in the set of shared users (the shared users are the only ones for which the information
     # can be transferred)
     for user in tqdm(range(n_sh_users)):
-        # take the top k items recommended for this user in the source domain (assuming k to be low and the recommender
-        # to be accurate, these should be items for which we have very accurate predictions in the source domain)
-        user_src_top_k = top_k_items[user]
-        # check which of these items are connected to at least one item in the target domain. Note also that the
-        # resulting items will be items with more than 300 ratings in the source domain (warm start items) cause the
-        # sim matrix only contains paths for items with more than 300 ratings in the source domain
-        filtered_user_src_top_k = [j for j in user_src_top_k if j in src_exist_path_items]
-        # check if this list is not empty, namely if there exists at least a source domain item that this user likes
-        # and is connected to at least one target domain item
-        # if this does not exist, then the user should not be considered as any paths will be included in the sim matrix
-        # for this user
-        if filtered_user_src_top_k:
-            # take all the items that have been positively interacted by the shared user in the target domain
-            non_zero_indices = tgt_ui_matrix[user].nonzero()[1]
-            # check if the user is a cold-start user
-            if len(non_zero_indices) <= 5:
-                # get all the items for which a rating is missing in the target domain for this cold-start user
-                total_indices = np.arange(tgt_ui_matrix.shape[1])
-                zero_indices = np.setdiff1d(total_indices, non_zero_indices)
+        # check if the user is cold-start in the source domain
+        user_src_interacted_items = src_ui_matrix[user].nonzero()[1]
+        if len(user_src_interacted_items) > 5:
+            # take the top k items recommended for this user in the source domain (assuming k to be low and the
+            # recommender to be accurate, these should be items for which we have very accurate predictions in the
+            # source domain)
+            user_src_top_k = top_k_items[user]
+            # check which of these items are connected to at least one item in the target domain. Note also that the
+            # resulting items will be items with more than 300 ratings in the source domain (warm start items) cause the
+            # sim matrix only contains paths for items with more than 300 ratings in the source domain
+            filtered_user_src_top_k = [j for j in user_src_top_k if j in src_exist_path_items]
+            # check if this list is not empty, namely if there exists at least a source domain item that this user likes
+            # and is connected to at least one target domain item
+            # if this does not exist, then the user should not be considered as any paths will be included in the
+            # sim matrix for this user
+            if filtered_user_src_top_k:
+                # take all the items that have been positively interacted by the shared user in the target domain
+                user_tgt_interacted_items = tgt_ui_matrix[user].nonzero()[1]
+                # check if the user is a cold-start user. We want to transfer information only to cold-start users in
+                # target domain as the model should have enough data for the other users to learn enough information
+                if len(user_tgt_interacted_items) <= 5:
+                    # get all the items for which a rating is missing in the target domain for this cold-start user
+                    all_items = np.arange(tgt_ui_matrix.shape[1])
+                    no_rated_items = np.setdiff1d(all_items, user_tgt_interacted_items)
 
-                for tgt_item_id in zero_indices:
-                    # for each item, check if the item is connected with at least one source domain item
-                    if tgt_item_id in tgt_exist_path_items:
-                        # if the connection exists, we want to find to which source domain items liked by the shared
-                        # user this item is connected
+                    for tgt_item_id in no_rated_items:
+                        # for each item, check if the item is connected with at least one source domain item
+                        if tgt_item_id in tgt_exist_path_items:
+                            # if the connection exists, we want to find to which source domain items liked by the shared
+                            # user this item is connected
 
-                        # get paths for the top-k items recommended for the shared user
-                        user_src_tgt_paths = sim_matrix[filtered_user_src_top_k]
-                        # for each possible path from each of the top-k source items for the shared user, we check if
-                        # the path converges into the current target item
-                        for src_item_id, user_paths in zip(filtered_user_src_top_k, user_src_tgt_paths):
-                            if tgt_item_id in user_paths.nonzero()[1]:
-                                # if the current path converges into the current target item, we generate the triplet
-                                yield user, src_item_id, tgt_item_id
+                            # get paths for the top-k items recommended for the shared user
+                            user_src_tgt_paths = sim_matrix[filtered_user_src_top_k]
+                            # for each possible path from each of the top-k source items for the shared user, we check
+                            # if the path converges into the current target item
+                            for src_item_id, user_paths in zip(filtered_user_src_top_k, user_src_tgt_paths):
+                                if tgt_item_id in user_paths.nonzero()[1]:
+                                    # if the current path converges into the current target item, we generate the
+                                    # triplet
+                                    yield user, src_item_id, tgt_item_id
