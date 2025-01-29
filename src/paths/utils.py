67e56379137ff24c2cec3d5e288b8e2c5363d887
Nicolò Bertocco <nick.bertocco@gmail.com>
2025-01-29 17:05:32 +0100

    fixed non interactive logging

--- src/paths/utils.py
+++ src/paths/utils.py
@@ -4,9 +4,19 @@ import sys
 from typing import Any, Literal, Optional
 
 import pandas as pd
-import tqdm
+from tqdm_loggable.auto import tqdm
+from tqdm_loggable.tqdm_logging import tqdm_logging
 from joblib import Parallel
 from loguru import logger
+import datetime
+import logging
+
+fmt = f"%(filename)-20s:%(lineno)-4d %(asctime)s %(message)s"
+logging.basicConfig(level=logging.INFO, format=fmt, handlers=[logging.StreamHandler()])
+
+# Set the rate how often we update logs
+# Defaults to 10 seconds - optional
+tqdm_logging.set_log_rate(datetime.timedelta(seconds=10))
 
 
 def get_rating_stats(
@@ -149,7 +159,7 @@ class ParallelTqdm(Parallel):
         disable_progressbar=False,
         show_joblib_header=False,
         file=sys.stderr,
-        **kwargs
+        **kwargs,
     ):
         if "verbose" in kwargs:
             raise ValueError(
@@ -160,7 +170,7 @@ class ParallelTqdm(Parallel):
         self.total_tasks = total_tasks
         self.desc = desc
         self.disable_progressbar = disable_progressbar
-        self.progress_bar: Optional[tqdm.tqdm] = None
+        self.progress_bar: Optional[tqdm] = None
         self.file = file
 
     def __call__(self, iterable):
@@ -183,13 +193,13 @@ class ParallelTqdm(Parallel):
     def dispatch_one_batch(self, iterator):
         # start progress_bar, if not started yet.
         if self.progress_bar is None:
-            self.progress_bar = tqdm.tqdm(
+            self.progress_bar = tqdm(
                 desc=self.desc,
                 total=self.total_tasks,
                 disable=self.disable_progressbar,
                 unit="tasks",
                 dynamic_ncols=True,
-                file=self.file
+                unit_scale=True,
             )
         # call parent function
         return super().dispatch_one_batch(iterator)
