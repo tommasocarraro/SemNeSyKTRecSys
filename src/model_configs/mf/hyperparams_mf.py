b486947669e9a4c7c69b1ed86feedb900e8cfe66
Nicolò Bertocco <nick.bertocco@gmail.com>
2025-03-03 14:16:33 +0100

    uploaded mf hyperparams

--- src/model_configs/mf/hyperparams_mf.py
+++ src/model_configs/mf/hyperparams_mf.py
@@ -2,9 +2,12 @@ from src.data_preprocessing.Dataset import Domains_Type
 from src.model_configs.mf.ModelConfigMf import HyperParamsMf
 
 hyperparams_dict = {
-    "books": HyperParamsMf(n_factors=1, batch_size=64, learning_rate=0.01, weight_decay=0.01),
-    "movies": HyperParamsMf(n_factors=1, batch_size=512, learning_rate=0.01, weight_decay=0.01),
-    "music": HyperParamsMf(n_factors=1, batch_size=64, learning_rate=0.01, weight_decay=0.01),
+    "movies": HyperParamsMf(
+        n_factors=100, batch_size=512, learning_rate=0.0006856905672627633, weight_decay=0.09061932384191268
+    ),
+    "music": HyperParamsMf(
+        n_factors=200, batch_size=512, learning_rate=0.0005219038500094297, weight_decay=0.09990886349746377
+    ),
 }
 
 
